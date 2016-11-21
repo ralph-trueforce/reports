4aa0317452d413af8ec85483d460b2eb3e18585d
ralph <ralph.asendeteufrer@trueforce.com>
2016-11-21 14:42:13 -0800

    Add buttons to add many graphics

--- demo/dashboard/script.js
+++ demo/dashboard/script.js
@@ -3,8 +3,16 @@ angular.module('app')
 .directive( 'getSize', function() {
 	return {
 		link: function( scope, element, attrs ) {
+
 			/**
-			 * Work-around:  check if the id has a angular model variable
+			 * Called after a widget is loaded
+			 */
+			element.ready(function(){
+				updateGraph();
+			});
+
+			/**
+			 * Work-around: Check if the id has a angular model variable
 			 * @param string
 			 * @returns {boolean}
 			 */
@@ -19,9 +27,12 @@ angular.module('app')
 				element.css('height', (element[0].parentElement.clientHeight - 100) + 'px');
 				var ID = element[0].id;
 				if (isNaN(ID) && !isAngularModelVar(ID)) {
-					console.log(ID);
+					var _Class = ID.split("_")[0];
+					if (_Class == 'Html') {
+						return;
+					}
 					angular.element(document.querySelector("#" + ID)).empty();
-					eval("var graph = new " + ID + "(" + element[0].clientWidth + ", " + (element[0].clientHeight) + ");");
+					eval("var graph = new " + _Class + "(" + element[0].clientWidth + ", " + (element[0].clientHeight) + ");");
 					graph.draw("#" + ID);
 				}
 			}
@@ -42,19 +53,7 @@ angular.module('app')
 			scope.$on('gridster-item-transition-end', updateGraph);
 			scope.$on('gridster-resized',             updateGraph);
 			scope.$on('gridster-resizable-changed',   updateGraph);
-		}
-	};
-})
-
-.directive( 'dragme', function() {
-	return {
-		link: function( scope, element, attrs ) {
-			element.bind('click', function() {
-				alert("click me");
-			});
-			element.bind('mouseup', function() {
-				alert("release me");
-			});
+			scope.$on('gridster-item-initialized',    updateGraph);
 		}
 	};
 })
@@ -90,16 +89,16 @@ angular.module('app')
 					sizeY: 1,
 					sizeX: 1,
 					name: "Widget",
-					id: "Donut",
-					type: 'graph'
+					id: 'Donut_1',
+					type: 'Donut'
 				}, {
 					col: 2,
 					row: 1,
 					sizeY: 1,
 					sizeX: 1,
 					name: "Widget",
-					id: "Lines",
-					type: 'graph'
+					id: 'Lines_1',
+					type: 'Lines'
 				}]
 			},
 			'2': {
@@ -110,13 +109,17 @@ angular.module('app')
 					row: 1,
 					sizeY: 1,
 					sizeX: 2,
-					name: "Other Widget 1"
+					name: "Other Widget 1",
+					id: 'Lines_1',
+					type: 'Lines'
 				}, {
 					col: 1,
 					row: 3,
 					sizeY: 1,
 					sizeX: 1,
-					name: "Other Widget 2"
+					name: "Other Widget 2",
+					id: 'Donut_1',
+					type: 'Donut'
 				}]
 			}
 		};
@@ -125,22 +128,28 @@ angular.module('app')
 			$scope.dashboard.widgets = [];
 		};
 
-		$scope.addWidget = function() {
-			if (localStorage['widget_index']) {
-				widgetIndex = localStorage['widget_index'];
+		$scope.addWidget = function(type) {
+			if (localStorage[type + '_index']) {
+				widgetIndex = localStorage[type + '_index'];
 				widgetIndex++;
 			} else {
-				widgetIndex = 3;
+				widgetIndex = 2;
 			}
-			localStorage['widget_index'] = widgetIndex;
+			localStorage[type + '_index'] = widgetIndex;
+
+			var html_text;
+			/*if (type == 'Html') {
+				html_text = $sce.trustAsHtml("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt risus luctus lobortis vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris bibendum leo a congue mattis. In blandit finibus nibh. Etiam tincidunt volutpat mollis. Integer dolor purus, ullamcorper nec purus et, semper interdum ligula. Curabitur magna mauris, feugiat sit amet convallis sagittis, pellentesque eget erat. Proin non laoreet elit. Vivamus posuere orci quis dolor rutrum ornare. Etiam convallis efficitur nulla eu scelerisque. Pellentesque congue accumsan turpis sit amet rutrum.</p><p>Aliquam vel massa tempor turpis auctor accumsan. Praesent et lacinia orci. Nulla dignissim quis ligula non fermentum. Pellentesque dictum mollis posuere. Sed congue eros eget euismod ornare. Integer id venenatis metus. Donec porttitor tincidunt lectus sed facilisis. Etiam condimentum, nulla et faucibus luctus, quam mi mollis metus, finibus tempor massa nisl at nisi. Suspendisse sodales pulvinar urna sodales maximus. Phasellus egestas quam ex, sed blandit eros finibus aliquam. Ut nec tellus rhoncus, tempor magna sed, volutpat ipsum.</p>");
+			}*/
 
 			$scope.dashboard.widgets.push({
 				name: "New Widget",
 				sizeX: 1,
 				sizeY: 1,
-				id: widgetIndex,
-				text: $sce.trustAsHtml("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt risus luctus lobortis vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris bibendum leo a congue mattis. In blandit finibus nibh. Etiam tincidunt volutpat mollis. Integer dolor purus, ullamcorper nec purus et, semper interdum ligula. Curabitur magna mauris, feugiat sit amet convallis sagittis, pellentesque eget erat. Proin non laoreet elit. Vivamus posuere orci quis dolor rutrum ornare. Etiam convallis efficitur nulla eu scelerisque. Pellentesque congue accumsan turpis sit amet rutrum.</p><p>Aliquam vel massa tempor turpis auctor accumsan. Praesent et lacinia orci. Nulla dignissim quis ligula non fermentum. Pellentesque dictum mollis posuere. Sed congue eros eget euismod ornare. Integer id venenatis metus. Donec porttitor tincidunt lectus sed facilisis. Etiam condimentum, nulla et faucibus luctus, quam mi mollis metus, finibus tempor massa nisl at nisi. Suspendisse sodales pulvinar urna sodales maximus. Phasellus egestas quam ex, sed blandit eros finibus aliquam. Ut nec tellus rhoncus, tempor magna sed, volutpat ipsum.</p>"),
-				type: 'html'
+				id: type + '_' + widgetIndex,
+				text: html_text,
+				type: type,
+				state: 'new'
 			});
 		};
 
@@ -177,7 +186,6 @@ angular.module('app')
 				}
 			});
 		};
-
 	}
 ])
 
