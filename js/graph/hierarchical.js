b390a4dee59fb710896ca2a14c93a95655a048c8
ralph <ralph.asendeteufrer@trueforce.com>
2016-12-12 12:13:16 -0400

    Change on hirarchical graph, inherited from cardinal class now

--- js/graph/hierarchical.js
+++ js/graph/hierarchical.js
@@ -9,30 +9,22 @@
  * @constructor
  */
 function Hierarchical(width, height) {
-	this.base = Graphic;
+	this.base = Cartesian;
 	this.base(width, height); //call super constructor.
-	//Graphic.call(width, height);
+	this.name = arguments.callee.name.toLowerCase();
 
     /**
      * Draw function member
      * @param tag_id
      */
-    this.draw = function(tag_id) {
+    this.process = function(tag_id) {
+    	var _this = this;
         // Set the dimensions of the canvas / graph
-        var margin = {top: 30, right: 50, bottom: 30, left: 50},
-            width = this.width - margin.left - margin.right,
-            height = this.height - margin.top - margin.bottom;
+        var width = this.width - this.margin.left - this.margin.right,
+            height = this.height - this.margin.top - this.margin.bottom;
 
         var x = d3.scale.linear()
             .range([0, width]);
-
-        var barHeight = 20;
-
-        var color = d3.scale.ordinal()
-            .range(["steelblue", "#ccc"]);
-
-        var duration = 750,
-            delay = 25;
 
         var partition = d3.layout.partition()
             .value(function (d) {
@@ -44,10 +36,12 @@ function Hierarchical(width, height) {
             .orient("top");
 
         var svg = d3.select(tag_id).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width + this.margin.left + this.margin.right)
+            .attr("height", height + this.margin.top + this.margin.bottom)
             .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+		d3.select(tag_id).style("background-color", this.config.background_color);
 
         svg.append("rect")
             .attr("class", "background")
@@ -73,7 +67,7 @@ function Hierarchical(width, height) {
 
         function down(d, i) {
             if (!d.children || this.__transition__) return;
-            var end = duration + d.children.length * delay;
+            var end = _this.config.duration + d.children.length * _this.config.delay;
 
             // Mark any currently-displayed bars as exiting.
             var exit = svg.selectAll(".enter")
@@ -94,7 +88,7 @@ function Hierarchical(width, height) {
             // Have the text fade-in, even though the bars are visible.
             // Color the bars as parents; they will fade to children if appropriate.
             enter.select("text").style("fill-opacity", 1e-6);
-            enter.select("rect").style("fill", color(true));
+            enter.select("rect").style("fill", _this.color(true));
 
             // Update the x-scale domain.
             x.domain([0, d3.max(d.children, function (d) {
@@ -103,17 +97,17 @@ function Hierarchical(width, height) {
 
             // Update the x-axis.
             svg.selectAll(".x.axis").transition()
-                .duration(duration)
+                .duration(_this.config.duration)
                 .call(xAxis);
 
             // Transition entering bars to their new position.
             var enterTransition = enter.transition()
-                .duration(duration)
+                .duration(_this.config.duration)
                 .delay(function (d, i) {
-                    return i * delay;
+                    return i * _this.config.delay;
                 })
                 .attr("transform", function (d, i) {
-                    return "translate(0," + barHeight * i * 1.2 + ")";
+                    return "translate(0," + _this.config.barHeight * i * 1.2 + ")";
                 });
 
             // Transition entering text.
@@ -126,12 +120,12 @@ function Hierarchical(width, height) {
                     return x(d.value);
                 })
                 .style("fill", function (d) {
-                    return color(!!d.children);
+                    return _this.color(!!d.children);
                 });
 
             // Transition exiting bars to fade out.
             var exitTransition = exit.transition()
-                .duration(duration)
+                .duration(_this.config.duration)
                 .style("opacity", 1e-6)
                 .remove();
 
@@ -152,7 +146,7 @@ function Hierarchical(width, height) {
 
         function up(d) {
             if (!d.parent || this.__transition__) return;
-            var end = duration + d.children.length * delay;
+            var end = _this.config.duration + d.children.length * _this.config.delay;
 
             // Mark any currently-displayed bars as exiting.
             var exit = svg.selectAll(".enter")
@@ -161,7 +155,7 @@ function Hierarchical(width, height) {
             // Enter the new bars for the clicked-on data's parent.
             var enter = bar(d.parent)
                 .attr("transform", function (d, i) {
-                    return "translate(0," + barHeight * i * 1.2 + ")";
+                    return "translate(0," + _this.config.barHeight * i * 1.2 + ")";
                 })
                 .style("opacity", 1e-6);
 
@@ -169,7 +163,7 @@ function Hierarchical(width, height) {
             // Exiting nodes will obscure the parent bar, so hide it.
             enter.select("rect")
                 .style("fill", function (d) {
-                    return color(!!d.children);
+                    return _this.color(!!d.children);
                 })
                 .filter(function (p) {
                     return p === d;
@@ -183,7 +177,7 @@ function Hierarchical(width, height) {
 
             // Update the x-axis.
             svg.selectAll(".x.axis").transition()
-                .duration(duration)
+                .duration(_this.config.duration)
                 .call(xAxis);
 
             // Transition entering bars to fade in over the full duration.
@@ -203,9 +197,9 @@ function Hierarchical(width, height) {
 
             // Transition exiting bars to the parent's position.
             var exitTransition = exit.selectAll("g").transition()
-                .duration(duration)
+                .duration(_this.config.duration)
                 .delay(function (d, i) {
-                    return i * delay;
+                    return i * _this.config.delay;
                 })
                 .attr("transform", stack(d.index));
 
@@ -218,7 +212,7 @@ function Hierarchical(width, height) {
                 .attr("width", function (d) {
                     return x(d.value);
                 })
-                .style("fill", color(true));
+                .style("fill", _this.color(true));
 
             // Remove exiting nodes when the last child has finished transitioning.
             exit.transition()
@@ -247,7 +241,7 @@ function Hierarchical(width, height) {
 
             bar.append("text")
                 .attr("x", -6)
-                .attr("y", barHeight / 2)
+                .attr("y", _this.config.barHeight / 2)
                 .attr("dy", ".35em")
                 .style("text-anchor", "end")
                 .text(function (d) {
@@ -258,7 +252,7 @@ function Hierarchical(width, height) {
                 .attr("width", function (d) {
                     return x(d.value);
                 })
-                .attr("height", barHeight);
+                .attr("height", _this.config.barHeight);
 
             return bar;
         }
@@ -267,7 +261,7 @@ function Hierarchical(width, height) {
         function stack(i) {
             var x0 = 0;
             return function (d) {
-                var tx = "translate(" + x0 + "," + barHeight * i * 1.2 + ")";
+                var tx = "translate(" + x0 + "," + _this.config.barHeight * i * 1.2 + ")";
                 x0 += x(d.value);
                 return tx;
             };
@@ -276,4 +270,4 @@ function Hierarchical(width, height) {
     };
 }
 
-Hierarchical.prototype = Object.create(Graphic.prototype);
+Hierarchical.prototype = Object.create(Cartesian.prototype);
